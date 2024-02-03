3db5067096514d623a34a58d11545b3eea53cc9d
Megan-J <mmju2103@gmail.com>
2024-02-02 22:31:30 -0800

    updated color pallete

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
 
-      <div className="group rounded-lg text-4xl text-edge-outline whitespace-nowrap bg-clip-text ">
-        <h1>Megan Ju</h1>
+      <div className="text-center text-7xl font-bold lg:max-w-2xl lg:w-full lg:mb-0">
+        Megan Ju
       </div>
 
       <div className="my-16 text-center ">
-        <h2 className={`m-0 text-sm opacity-50`}>
+        <h2 className={`m-0 text-md opacity-50`}>
           Software developer hard at work
         </h2>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-2xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
         <Link
           href="/experience"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -44,19 +44,6 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about projects I've worked on!
           </p>
-        </Link>
-
-        <Link
-          href="/contact"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Contact{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Say hi!</p>
         </Link>
       </div>
     </main>
