6b8fe3655b17eac1e32da21de5cf4beb16fdceec
Megan-J <mmju2103@gmail.com>
2024-02-02 22:33:48 -0800

    changed titles

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="text-center text-5xl flex min-h-screen flex-col items-center justify-between p-24">
       <div>Projects</div>
     </main>
   );
