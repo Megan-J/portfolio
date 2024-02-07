003faf3964708f00e2bff4578865643283b2fbc0
Megan-J <mmju2103@gmail.com>
2024-02-07 12:43:42 -0800

    added timeline

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,11 +1,19 @@
-import { LinkedIn, GitHub, Email } from "@mui/icons-material";
+import { Description, LinkedIn, GitHub, Email } from "@mui/icons-material";
 import Link from "@mui/material/Link";
 //import Link from "next/link";
 
 export default function Socials() {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <Link
+          href=""
+          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          target="_blank"
+        >
+          <Description sx={{ fontSize: 48 }}></Description>
+        </Link>
+
         <Link
           href="https://www.linkedin.com/in/meganju/"
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,9 +1,63 @@
 import Link from "next/link";
 
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineOppositeContent,
+} from "@mui/lab";
+
 export default function About() {
   return (
     <main className="text-center text-5xl flex min-h-screen flex-col items-center justify-between p-24">
       <div>Experience</div>
+
+      <div className="items-center">
+        <Timeline>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              May 2023 - August 2023
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>R&D Software Developer Intern</TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              May 2022 - August 2022
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>REU Intern</TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              June 2022 - May 2023
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>Program Manager</TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              June 2022 - May 2023
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+            </TimelineSeparator>
+            <TimelineContent>Program Manager</TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </div>
     </main>
   );
 }
