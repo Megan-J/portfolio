85f50e28e73d2b1741ab5909431f2495edd23089
Megan-J <mmju2103@gmail.com>
2024-02-07 12:20:00 -0800

    working links

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,24 +1,34 @@
 import { LinkedIn, GitHub, Email } from "@mui/icons-material";
-import Link from "next/link";
+import Link from "@mui/material/Link";
+//import Link from "next/link";
 
 export default function Socials() {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
       <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        <LinkedIn
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          sx={{ fontSize: 48 }}
+        <Link
           href="https://www.linkedin.com/in/meganju/"
-        ></LinkedIn>
-        <GitHub
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          sx={{ fontSize: 48 }}
+          target="_blank"
+        >
+          <LinkedIn sx={{ fontSize: 48 }}></LinkedIn>
+        </Link>
+
+        <Link
           href="https://github.com/Megan-J"
-        ></GitHub>
-        <Email
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          sx={{ fontSize: 48 }}
-        ></Email>
+          target="_blank"
+        >
+          <GitHub sx={{ fontSize: 48 }}></GitHub>
+        </Link>
+
+        <Link
+          href="mailto:mmju2103@gmail.com"
+          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          target="_blank"
+        >
+          <Email sx={{ fontSize: 48 }}></Email>
+        </Link>
       </div>
     </div>
   );
