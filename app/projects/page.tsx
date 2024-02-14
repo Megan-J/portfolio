ebe30d520f10797c3ba890ec779906092a137ae7
Megan-J <mmju2103@gmail.com>
2024-02-13 17:33:25 -0800

    setup for projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import thumbnailData from "./thumbnailData";
 
 export default function About() {
   return (
---
+++ app/projects/thumbnailData.ts
@@ -0,0 +1,19 @@
+interface Project {
+    title: string,
+    description: string,
+    href?: string,
+    imgSrc?: string,
+  }
+  
+  const thumbnailData: Project[] = [
+    {
+      title: 'Project 1',
+      description: `Descriptions`,
+    },
+    {
+      title: 'Project 1',
+      description: `Template Text`,
+    },
+  ]
+  
+  export default thumbnailData
